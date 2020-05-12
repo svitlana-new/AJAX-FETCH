3058a0c00b69db1a2631a5a1b0d40eef385d1725
svitlana-new <galka.mospan@gmail.com>
2020-05-12 13:05:43 +0000

    created ajax/fetch samples

---
+++ js/index.js
@@ -0,0 +1,54 @@
+document.querySelector('.time').innerText
+  = new Date().toLocaleTimeString();
+
+document.querySelector('.get-ajax-html').addEventListener('click', getAjaxHtml);
+
+function getAjaxHtml () {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          document.querySelector('.html-container')
+            .innerHTML = xhr.responseText;
+      }
+  }
+  xhr.open('get', 'client-data.html', true);
+  xhr.send();
+} 
+
+document.querySelector('.fetch-html').addEventListener('click', getFetchHtml);
+
+// function getFetchHtml () {
+//     fetch('client-data.html')
+//     .then(response => response.text())
+//     .then(html => document.querySelector('.html-container')
+//             .innerHTML = html);
+// }
+
+async function getFetchHtml () {
+    const response = await fetch('client-data.html');
+    const html = await response.text();
+    document.querySelector('.html-container')
+          .innerHTML = html
+}
+
+document.querySelector('.get-ajax-json').addEventListener('click', getAjaxJson);
+
+function getAjaxJson () {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        document.querySelector('.name').innerText = data.name;
+        document.querySelector('.balance').innerText = data.balance;
+      }
+    }
+  xhr.open('get', 'client-data.json', true);
+  xhr.send();
+}
+document.querySelector('.fetch-json').addEventListener('click', getFetchJson);
+async function getFetchJson () {
+    const response = await fetch('client-data.json');
+    const data = await response.json();
+    document.querySelector('.name').innerText = data.name;
+    document.querySelector('.balance').innerText = data.balance;
+}
