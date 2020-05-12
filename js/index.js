d1d72fc949748546bc03ef469635996ca9b9fe93
svitlana-new <galka.mospan@gmail.com>
2020-05-12 15:29:32 +0000

    added backend elements

--- js/index.js
+++ js/index.js
@@ -52,3 +52,20 @@ async function getFetchJson () {
     document.querySelector('.name').innerText = data.name;
     document.querySelector('.balance').innerText = data.balance;
 }
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            password: document.querySelector('.login-form input[name=password]').value
+        })
+    })
+    .then(_ => document.querySelector('.login-form').reset());
+}
